2260ea9e4bfbb3463787b61bba223d38cd57d80a
Fernando Craveiro <31604881+fcraveiro@users.noreply.github.com>
2022-01-31 08:07:44 -0300

    Update conectar.dart

--- lib/services/conectar.dart
+++ lib/services/conectar.dart
@@ -2,9 +2,8 @@ import 'dart:developer';
 import 'package:popquiz/model/model.dart';
 import 'package:supabase/supabase.dart';
 
-const supabaseUrl = 'https://xrhyhsbetlnzksauwrvi.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODA3NTI4MywiZXhwIjoxOTUzNjUxMjgzfQ.gsz31qxeQ_h6R_93rthZwynvz1i8jNrXLz30JaFprqA';
+const supabaseUrl = '';
+const supabaseKey = '';
 
 class Conecta {
   final client = SupabaseClient(supabaseUrl, supabaseKey);
